1d5bb5e66367d399077e2f467595b6c974791491
Qu5antum <erae88959@gmail.com>
2026-02-22 18:54:14 +0300

    face recognition and get user by face

--- src/app/api/endpoints/user_route.py
+++ src/app/api/endpoints/user_route.py
@@ -1,8 +1,11 @@
-from fastapi import APIRouter, status, Depends
+from fastapi import APIRouter, status, Depends, UploadFile, File
 from fastapi.security import OAuth2PasswordRequestForm
 
 from src.app.database.db import get_session, AsyncSession
+from src.app.database.models import User
 from src.app.service.user_service import auth_user
+from src.app.api.dependencies.dependency import get_current_user
+from src.app.service.face_setup_service import register_face_for_current_user, recognize_user_by_face
 
 user_route = APIRouter(
     prefix="/user",
@@ -16,3 +19,20 @@ async def login(
     session: AsyncSession = Depends(get_session)
 ):
     return await auth_user(session=session, credents=credents)
+
+
+@user_route.post("/register_face/{user_id}", status_code=status.HTTP_200_OK)
+async def register_face(
+    file: UploadFile = File(...),
+    user: User = Depends(get_current_user),
+    session: AsyncSession = Depends(get_session)
+):
+    return await register_face_for_current_user(session=session, user=user, file=file)
+
+
+@user_route.post("/recognize_face", status_code=status.HTTP_200_OK)
+async def recognize_face(
+    file: UploadFile = File(...),
+    session: AsyncSession = Depends(get_session)
+):
+    return await recognize_user_by_face(session=session, file=file)
--- src/app/service/face_setup_service.py
+++ src/app/service/face_setup_service.py
@@ -1,3 +1,99 @@
-import base64
 import face_recognition
+import numpy as np
+from PIL import Image
+import io
+from fastapi import UploadFile, HTTPException, status
+from sqlalchemy import select
+
+from src.app.database.db import AsyncSession
+from src.app.database.models import User
+
+async def register_face_for_current_user(
+        session: AsyncSession,
+        user: User,
+        file: UploadFile
+):
+    contents = await file.read()
+
+    image = Image.open(io.BytesIO(contents)).convert("RGB")
+    image = np.array(image, dtype=np.uint8)
+
+    encodings = face_recognition.face_encodings(image)
+
+    if not encodings:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST, 
+            detail="Face not detected."
+        )
+
+    if len(encodings) != 1:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST, 
+            detail="Exactly one face required."
+        )
+    
+    encoding = encodings[0]
+
+    encoding_bytes = encoding.tobytes()
+
+    user.face_encoding = encoding_bytes
+
+    await session.commit()
+
+    return {"message": "Face saved successfully"}
+
+
+async def recognize_user_by_face(
+        session: AsyncSession,
+        file: UploadFile
+):
+    contents = await file.read()
+
+    image = Image.open(io.BytesIO(contents)).convert("RGB")
+    image = np.array(image, dtype=np.uint8)
+
+    encodings = face_recognition.face_encodings(image)
+
+    if not encodings:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST, 
+            detail="Face not detected."
+        )
+
+    if len(encodings) != 1:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST, 
+            detail="Exactly one face required."
+        )
+    
+    input_encoding = encodings[0]
+
+    result = await session.execute(
+        select(User).where(User.face_encoding.is_not(None))
+    )
+    users = result.scalars().all()
+
+    for user in users:
+        db_encoding = np.frombuffer(user.face_encoding, dtype=np.float64)
+
+        distance = face_recognition.face_distance([db_encoding], input_encoding)[0]
+        
+        if distance < 0.5:
+            return {
+                "user_id": str(user.id),
+                "email": user.email
+            }
+    
+    raise HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED, 
+        detail="Face not recognized."
+    )
+
+
+
+
+    
+    
+    
+    
 
