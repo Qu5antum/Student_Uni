2ef165b026c98dc7c60f7bca9958a58d24bd3dfa
Qu5antum <erae88959@gmail.com>
2026-02-23 16:35:44 +0300

    update SectionService

--- src/app/api/endpoints/section_route.py
+++ src/app/api/endpoints/section_route.py
@@ -2,7 +2,7 @@ from fastapi import APIRouter, status, Depends
 
 from src.app.database.db import get_session, AsyncSession
 from src.app.api.schemas.section import SectionCreate
-from src.app.service.section_service import add_new_section, get_section_by_id, delete_section_by_id
+from src.app.service.section_service import SectionService
 from src.app.api.dependencies.check_role import require_roles
 
 section_route = APIRouter(
@@ -16,7 +16,8 @@ async def new_section(
     section: SectionCreate,
     session: AsyncSession = Depends(get_session)
 ):
-    return await add_new_section(session=session, section=section)
+    section_service = SectionService(session=session)
+    return await section_service.add_new_section(session=session, section=section)
 
 
 @section_route.get("/", dependencies=[Depends(require_roles(["ADMIN"]))], status_code=status.HTTP_200_OK)
@@ -24,7 +25,8 @@ async def get_section(
     faculty_id: int,
     session: AsyncSession = Depends(get_session)
 ):
-    return await get_section_by_id(session=session, faculty_id=faculty_id)
+    section_service = SectionService(session=session)
+    return await section_service.get_section_by_id(session=session, faculty_id=faculty_id)
 
 
 @section_route.get("/{section_id}", dependencies=[Depends(require_roles(["ADMIN"]))], status_code=status.HTTP_200_OK)
@@ -33,7 +35,8 @@ async def get_section(
     section_id: int,
     session: AsyncSession = Depends(get_session)
 ):
-    return await get_section_by_id(session=session, faculty_id=faculty_id, section_id=section_id)
+    section_service = SectionService(session=session)
+    return await section_service.get_section_by_id(session=session, faculty_id=faculty_id, section_id=section_id)
 
 
 @section_route.delete("/{section}", dependencies=[Depends(require_roles(["ADMIN"]))], status_code=status.HTTP_200_OK)
@@ -42,4 +45,5 @@ async def delete_section(
     section_id: int,
     session: AsyncSession = Depends(get_session)
 ):
-    return await delete_section_by_id(session=session, faculty_id=faculty_id, section_id=section_id)
+    section_service = SectionService(session=session)
+    return await section_service.delete_section_by_id(session=session, faculty_id=faculty_id, section_id=section_id)
--- src/app/service/section_service.py
+++ src/app/service/section_service.py
@@ -7,76 +7,111 @@ from src.app.database.models import Section, Faculty
 from src.app.api.schemas.section import SectionCreate
 
 
-async def add_new_section(
-        session: AsyncSession,
-        section: SectionCreate
-):
-    try:
-        existing_faculty = await session.get(Faculty, section.faculty_id)
+class SectionService:
+    def __init__(self, session: AsyncSession):
+        self.session = session
+        
+    async def add_new_section(
+            self,
+            section: SectionCreate
+    ):
+        try:
+            existing_faculty = await self.session.get(Faculty, section.faculty_id)
 
+            if not existing_faculty:
+                raise HTTPException(
+                    status_code=status.HTTP_404_NOT_FOUND,
+                    detail="Faculty not found."
+                )
+            
+            result = await self.session.execute(
+                select(Section).where(
+                    Section.name == section.name,
+                    Section.faculty_id == section.faculty_id
+                )
+            )
+
+            existing_section = result.scalar_one_or_none()
+
+            if existing_section:
+                raise HTTPException(
+                    status_code=status.HTTP_400_BAD_REQUEST,
+                    detail="Section already exists in this faculty."
+                )
+            
+            new_section = Section(
+                **section.model_dump()
+            )
+
+            self.session.add(new_section)
+            await self.session.commit()
+            await self.session.refresh(new_section)
+
+            return new_section
+        except IntegrityError:
+            await self.session.rollback()
+
+            raise HTTPException(
+                status_code=status.HTTP_400_BAD_REQUEST,
+                detail="Section already exists."
+            )
+        
+
+    async def get_section_by_id(
+            self,
+            faculty_id: int,
+            section_id: int | None = None
+    ):
+        existing_faculty = await self.session.get(Faculty, faculty_id)
+        
         if not existing_faculty:
             raise HTTPException(
                 status_code=status.HTTP_404_NOT_FOUND,
-                detail="Faculty not found."
+                detail=f"Faculty by this ID: {faculty_id} not found."
             )
         
-        result = await session.execute(
-            select(Section).where(
-                Section.name == section.name,
-                Section.faculty_id == section.faculty_id
+        if not section_id:
+            result = await self.session.execute(
+                select(Section).where(Section.faculty_id == faculty_id)
             )
-        )
 
-        existing_section = result.scalar_one_or_none()
+            sections = result.scalars().all()
+        
+            return sections
+        
+        elif section_id:
+            result = await self.session.execute(
+                select(Section).where(
+                    Section.id == section_id,
+                    Section.faculty_id == faculty_id
+                )
+            )
 
-        if existing_section:
+            existing_section = result.scalar_one_or_none()
+
+            if not existing_section:
+                raise HTTPException(
+                    status_code=status.HTTP_404_NOT_FOUND,
+                    detail=f"Section by this ID: {section_id} not found in this faculty."
+                )
+            
+            return existing_section
+        
+
+    async def delete_section_by_id(
+            self,
+            faculty_id: int,
+            section_id: int | None = None
+    ):
+        existing_faculty = await self.session.get(Faculty, faculty_id)
+        
+        if not existing_faculty:
             raise HTTPException(
-                status_code=status.HTTP_400_BAD_REQUEST,
-                detail="Section already exists in this faculty."
+                status_code=status.HTTP_404_NOT_FOUND,
+                detail=f"Faculty by this ID: {faculty_id} not found."
             )
         
-        new_section = Section(
-            **section.model_dump()
-        )
-
-        session.add(new_section)
-        await session.commit()
-        await session.refresh(new_section)
-
-        return new_section
-    except IntegrityError:
-        await session.rollback()
-
-        raise HTTPException(
-            status_code=status.HTTP_400_BAD_REQUEST,
-            detail="Section already exists."
-        )
-    
-
-async def get_section_by_id(
-        session: AsyncSession,
-        faculty_id: int,
-        section_id: int | None = None
-):
-    existing_faculty = await session.get(Faculty, faculty_id)
-    
-    if not existing_faculty:
-        raise HTTPException(
-            status_code=status.HTTP_404_NOT_FOUND,
-            detail=f"Faculty by this ID: {faculty_id} not found."
-        )
-    
-    if not section_id:
-        result = await session.execute(
-            select(Section).where(Section.faculty_id == faculty_id)
-        )
-
-        sections = result.scalars().all()
-    
-        return sections
-    
-    elif section_id:
-        result = await session.execute(
+        result = await self.session.execute(
             select(Section).where(
                 Section.id == section_id,
                 Section.faculty_id == faculty_id
@@ -91,44 +126,13 @@ async def get_section_by_id(
                 detail=f"Section by this ID: {section_id} not found in this faculty."
             )
         
-        return existing_section
-    
+        await self.session.delete(existing_section)
+        await self.session.commit()
 
-async def delete_section_by_id(
-        session: AsyncSession,
-        faculty_id: int,
-        section_id: int | None = None
-):
-    existing_faculty = await session.get(Faculty, faculty_id)
-    
-    if not existing_faculty:
-        raise HTTPException(
-            status_code=status.HTTP_404_NOT_FOUND,
-            detail=f"Faculty by this ID: {faculty_id} not found."
-        )
-    
-    result = await session.execute(
-        select(Section).where(
-            Section.id == section_id,
-            Section.faculty_id == faculty_id
-        )
-    )
+        return {"detail": "Section successfully deleted."}
+            
 
-    existing_section = result.scalar_one_or_none()
-
-    if not existing_section:
-        raise HTTPException(
-            status_code=status.HTTP_404_NOT_FOUND,
-            detail=f"Section by this ID: {section_id} not found in this faculty."
-        )
-    
-    await session.delete(existing_section)
-    await session.commit()
-
-    return {"detail": "Section successfully deleted."}
-        
-
-        
+            
 
 
 
